0c80ca027acce3e176bb122ef90d01c6190d12ca
S4kred <s4kred.arch@gmail.com>
2025-12-28 01:31:47 -0300

    fix: upgrade to next@16.1.1 (CVE-2025-66478) and fix tailwind 4 css syntax issues

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,6 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  eslint: {
-    // En producción NO ignorar errores
-    ignoreDuringBuilds: !isProd ? true : false,
-  },
   typescript: {
     // En producción NO ignorar errores
     ignoreBuildErrors: !isProd ? true : false,
